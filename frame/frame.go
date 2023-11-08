4bf3a1e8edaf6bd9edb489cc74c89c428c5c8f83
Yao Noel ACHI <achi.noel@hotmail.com>
2023-11-08 22:09:57 +0100

    implement RESP simplestring marshal/unmarshal

---
+++ frame/frame.go
@@ -0,0 +1,191 @@
+package frame
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var (
+	ErrInvalidSimpleString = errors.New("input contains invalid characters (CR or LF)")
+	ErrNotEnoughData       = errors.New("not enough data to decode a valid frame")
+)
+
+type Framer interface {
+	// Serialize returns a slice of bytes representation of this frame. It produces a slice of bytes
+	// readies to be transferred other the network.
+	Serialize() []byte
+
+	// Deserialize tries to read a frame from a buffer. It returns an error if no frame can be read from the buffer.
+	// In case an error occur, the bytes read until the error are discarded. The reason to discard is that the
+	//  buffer is reused to read multiple frames. So if there is an invalid frame, discarding it give the chance to
+	// read a good one in subsequent calls. Also note that the buffer is supposed to be filled with stream of data.
+	// Lastly, after a successful read, the read cursor is positioned after the bytes read for subsequent reads.
+	Deserialize(b *bytes.Buffer) error
+
+	// String makes a frame implement the Stringer interface.
+	String() string
+}
+
+type SimpleString struct {
+	value string
+}
+
+func NewSimpleString(s string) (*SimpleString, error) {
+	if strings.ContainsAny(s, "\r\n") {
+		return nil, ErrInvalidSimpleString
+	}
+	return &SimpleString{s}, nil
+}
+
+func (s *SimpleString) Serialize() []byte {
+	return []byte(s.String())
+}
+
+func (s *SimpleString) String() string {
+	return fmt.Sprintf("+%s\r\n", s.value)
+}
+
+// Deserialize
+// @TODO Simple string should not contain any CR or LF in the middle so frame should error in this case
+func (s *SimpleString) Deserialize(buff *bytes.Buffer) error {
+	if buff.Len() < 2 {
+		return ErrNotEnoughData
+	}
+	readBytes, err := buff.ReadBytes('\n')
+	if err != nil {
+		return err
+	}
+	if readBytes[len(readBytes)-2] != '\r' {
+		return ErrInvalidSimpleString
+	}
+	frameContent := string(readBytes[:len(readBytes)-2])
+	// if there is any CR in the middle of the string,
+	// it is not a frame of type simple string. We do not double-check for LF
+	// because from the first read, we are guaranteed to not have any LF in the middle
+	if strings.ContainsRune(frameContent, '\r') {
+		return ErrInvalidSimpleString
+	}
+	s.value = frameContent
+	return nil
+}
+
+//func (s *BulkStringFrame) Deserialize(buff *bytes.Buffer) error {
+//	if buff.Len() < 2 {
+//		return ErrNotEnoughData
+//	}
+//	tmpRead := make([]byte, 0, 128)
+//	for {
+//		bs, err := buff.ReadBytes('\n')
+//		if err != nil {
+//			return err
+//		}
+//		tmpRead = append(tmpRead, bs...)
+//		if tmpRead[len(tmpRead)-2] == '\r' {
+//			break
+//		}
+//	}
+//	s.value = string(tmpRead[:len(tmpRead)-2])
+//	return nil
+//}
+
+//type Error struct {
+//	value string
+//}
+
+//func (e Error) Serialize() []uint8 {
+//	return []uint8(e)
+//}
+//
+//func (e ErrorFrame) String() string {
+//	return "-" + string(e) + "\r\n"
+//}
+//
+//type IntegerFrame int64
+//
+//func (i IntegerFrame) Serialize() []uint8 {
+//	return []uint8(fmt.Sprint(i))
+//}
+//
+//func (i IntegerFrame) String() string {
+//	return ":" + strconv.Itoa(int(i)) + "\r\n"
+//}
+//
+//type BulkStringFrame string
+//
+//func (b BulkStringFrame) Serialize() []uint8 {
+//	return []uint8(b)
+//}
+//
+//func (b BulkStringFrame) String() string {
+//	frameStr := "$-1\r\n"
+//	if b != "" {
+//		frameStr = "$" + strconv.Itoa(len(b)) + "\r\n" + string(b) + "\r\n"
+//	}
+//	return frameStr
+//}
+//
+//type BoolFrame bool
+//
+//func (b BoolFrame) Serialize() []uint8 {
+//	return []uint8(fmt.Sprint(b))
+//}
+//
+//func (b BoolFrame) String() string {
+//	bValue := bool(b)
+//	if bValue {
+//		return "#t\r\n"
+//	}
+//	return "#f\r\n"
+//}
+//
+//type NullFrame string
+//
+//func (n NullFrame) Serialize() []uint8 {
+//	return []uint8(n)
+//}
+//
+//func (n NullFrame) String() string {
+//	return "_\r\n"
+//}
+
+// example to decode frames based on the type
+//
+//
+// Define your Framer interface
+//type Framer interface {
+//	Deserialize(b *bytes.Buffer) error
+//	// other methods...
+//}
+//
+//// Suppose you have two different frame types
+//type FrameType1 struct { /* fields */ }
+//type FrameType2 struct { /* fields */ }
+//
+//// You define Deserialize differently for each type
+//func (f *FrameType1) Deserialize(b *bytes.Buffer) error {
+//	// deserialization for FrameType1
+//	// ...
+//	return nil
+//}
+//
+//func (f *FrameType2) Deserialize(b *bytes.Buffer) error {
+//	// deserialization for FrameType2
+//	// ...
+//	return nil
+//}
+//
+//// Then, in your function dealing with the incoming data
+//switch frameTypeByte {
+//case byte1:
+//frame := &FrameType1{}
+//err := frame.Deserialize(b)
+//// handle error and use the frame
+//case byte2:
+//frame := &FrameType2{}
+//err := frame.Deserialize(b)
+//// handle error and use the frame
+//default:
+//// handle error: unrecognized frame type
+//}
---
+++ frame/frame_test.go
@@ -0,0 +1,227 @@
+package frame
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestSimpleString_String(t *testing.T) {
+	tests := []struct {
+		name string
+		give string
+		want string
+	}{
+		{
+			name: "simple string should work",
+			give: "OK",
+			want: "+OK\r\n",
+		},
+		{
+			name: "empty string should work",
+			give: "",
+			want: "+\r\n",
+		},
+		{
+			name: "integer based string should work",
+			give: "732",
+			want: "+732\r\n",
+		},
+		{
+			name: "non ascii simple string should work",
+			give: "æ",
+			want: "+æ\r\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &SimpleString{
+				value: tt.give,
+			}
+			if got := fmt.Sprint(s); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSimpleString_Serialize(t *testing.T) {
+	tests := []struct {
+		name string
+		give string
+		want []byte
+	}{
+		{
+			name: "simple string should work",
+			give: "OK",
+			want: []byte("+OK\r\n"),
+		},
+		{
+			name: "empty string should work",
+			give: "",
+			want: []byte("+\r\n"),
+		},
+		{
+			name: "integer based string should work",
+			give: "732",
+			want: []byte("+732\r\n"),
+		},
+		{
+			name: "non ascii simple string should work",
+			give: "æ",
+			want: []byte("+æ\r\n"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &SimpleString{
+				value: tt.give,
+			}
+			if got := s.Serialize(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Serialize() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewSimpleString(t *testing.T) {
+	tests := []struct {
+		name    string
+		give    string
+		want    *SimpleString
+		wantErr bool
+	}{
+		{
+			name:    "a string without CR or LF should work",
+			give:    "OK",
+			want:    &SimpleString{"OK"},
+			wantErr: false,
+		},
+		{
+			name:    "a string with CRLF should fail",
+			give:    "OK\r\n",
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "a string with CR should fail",
+			give:    "O\rK",
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "a string with LF should fail",
+			give:    "OK\n",
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewSimpleString(tt.give)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewSimpleString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewSimpleString() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSimpleString_Deserialize(t *testing.T) {
+	tests := []struct {
+		name      string
+		give      *bytes.Buffer
+		wantFrame SimpleString
+		wantErr   bool
+	}{
+		{
+			name:      "basic working frame",
+			give:      bytes.NewBufferString("hello\r\n"),
+			wantFrame: SimpleString{value: "hello"},
+			wantErr:   false,
+		},
+		{
+			name:      "basic working frame with data left in the buffer",
+			give:      bytes.NewBufferString("hello\r\nworld"),
+			wantFrame: SimpleString{value: "hello"},
+			wantErr:   false,
+		},
+		{
+			name:      "working frame should not contain CR in the middle",
+			give:      bytes.NewBufferString("hel\rlo\r\n"),
+			wantFrame: SimpleString{},
+			wantErr:   true,
+		},
+		{
+			name:      "working frame should not contain LF in the middle",
+			give:      bytes.NewBufferString("hel\nlo\r\n"),
+			wantFrame: SimpleString{},
+			wantErr:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &SimpleString{}
+			err := s.Deserialize(tt.give)
+			if *s != tt.wantFrame {
+				t.Errorf("Deserialize() got = %v, want %v", *s, tt.wantFrame)
+			}
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Deserialize() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+//func TestBulkString_Deserialize(t *testing.T) {
+//	tests := []struct {
+//		name      string
+//		give      *bytes.Buffer
+//		wantFrame SimpleString
+//		wantErr   bool
+//	}{
+//		// TODO: Add test cases.
+//		{
+//			name:      "basic working frame",
+//			give:      bytes.NewBufferString("hello\r\n"),
+//			wantFrame: SimpleString{value: "hello"},
+//			wantErr:   false,
+//		},
+//		{
+//			name:      "basic working frame with data left in the buffer",
+//			give:      bytes.NewBufferString("hello\r\nworld"),
+//			wantFrame: SimpleString{value: "hello"},
+//			wantErr:   false,
+//		},
+//		{
+//			name:      "working frame can contain CR in the middle",
+//			give:      bytes.NewBufferString("hel\rlo\r\n"),
+//			wantFrame: SimpleString{value: "hel\rlo"},
+//			wantErr:   false,
+//		},
+//		{
+//			name:      "working frame can contain LF in the middle",
+//			give:      bytes.NewBufferString("hel\nlo\r\n"),
+//			wantFrame: SimpleString{value: "hel\nlo"},
+//			wantErr:   false,
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			s := &SimpleString{}
+//			err := s.Deserialize(tt.give)
+//			if *s != tt.wantFrame {
+//				t.Errorf("Deserialize() got = %v, want %v", *s, tt.wantFrame)
+//			}
+//			if (err != nil) != tt.wantErr {
+//				t.Errorf("Deserialize() error = %v, wantErr %v", err, tt.wantErr)
+//				return
+//			}
+//		})
+//	}
+//}
